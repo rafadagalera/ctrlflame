bb859005b49f491515131571da0895af04c7df51
dagalera <dagalera.dev@gmail.com>
2025-05-28 17:30:14 -0300

    Initial commit

---
+++ src/main/java/br/com/ctrlflame/CtrlFlameApplication.java
@@ -0,0 +1,13 @@
+package br.com.ctrlflame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrlFlameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtrlFlameApplication.class, args);
+	}
+
+}
